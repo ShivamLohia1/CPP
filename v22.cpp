8d64cc73bc4fe00039685445b0e3399a65d2c55f
Shivam Lohia <contactshivamlohia@gmail.com>
2025-06-16 21:32:55 +0530

    Add files via upload

---
+++ v22.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a;
+    cin >> a;
+
+    if(a>0){
+        cout << "a is positive" << endl;
+    }
+    else{
+        cout << "a is not positive" << endl;
+    }
+    return 0;
+}
