47a15c20fac92b137530c6bfe2e8361fceef147d
Shivam Lohia <contactshivamlohia@gmail.com>
2025-06-18 17:15:25 +0530

    Add files via upload

---
+++ v23.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(void){
+    int a;
+    cin >> a;
+    int b;
+    cin >> b;
+    if(a>b){
+        cout << "a is greater than b" << endl;
+    }
+    else{
+        cout << "b is greater than a" << endl;
+    }
+    return 0;
+}
