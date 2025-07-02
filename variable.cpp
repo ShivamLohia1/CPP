6d4950e2626b0df1eec39163717e8f8cbb40854c
Shivam Lohia <contactshivamlohia@gmail.com>
2025-07-02 19:14:58 +0530

    Add files via upload

---
+++ variable.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int pincode = 456789;
+    int age = 23;
+    int state = 36;
+
+    cout<<pincode<<endl;
+    cout<<age<<endl;
+    cout<<state<<endl;
+
+    float bank_balance = 456783;
+    float salary = 56778;
+
+    cout << "bank_balance = "<<bank_balance<<endl;
+    cout << "Salary = "<< salary<<endl;
+
+    char symbol1 = 'A';
+    char symbol2 = '1';
+    char symbol3 = 'B';
+
+    cout << "symbol1 = " << symbol1 << endl;
+    cout << "symbol2 = " << symbol2 << endl;    
+    cout << "symbol3 = " << symbol3 << endl;
+
+    string name = "Shivam Lohia";
+    string str = "!@#$%^&*()"; 
+    string country = "USA";
+
+    cout << "Name = " << name << endl;
+    cout << "Str = " << str << endl;
+    cout << "Country = " << country << endl;
+
+    return 0;
+}
