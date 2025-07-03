198a954218f328660a4435ba49cfe1d320989914
Shivam Lohia <contactshivamlohia@gmail.com>
2025-07-03 09:41:52 +0530

    Add files via upload

---
+++ user_input.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    int b;
+
+    cout<<"Enter value of a : ";
+    cin>>a;
+    cout<<"Enter value of b : ";  
+    cin>>b;
+    cout<<"Sum = "<<a+b<<endl;
+}
