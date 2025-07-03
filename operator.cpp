50625a19b0808c4041062c1e6453213844510065
Shivam Lohia <contactshivamlohia@gmail.com>
2025-07-03 09:41:19 +0530

    Add files via upload

---
+++ operator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 13;
+    int b = 67;
+
+    cout<<"Sum = "<<a+b<<endl; 
+    cout<<"Difference = "<<a-b<<endl;
+    cout<<"Product = "<<a*b<<endl; 
+    cout<<"Division = "<<(float)a/b<<endl; 
+    // arithmatic operator : {+,-,*,/,%}
+
+}
