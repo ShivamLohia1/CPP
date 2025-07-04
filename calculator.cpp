726fb5f67e0347a80db781fe8f75690e667c26e7
Shivam Lohia <contactshivamlohia@gmail.com>
2025-07-04 22:10:47 +0530

    Add files via upload

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int n;
+    cout << "Enter a 1st no. : " << endl;
+    cin >> n;
+    int p ;
+    cout << "Enter a 2nd no. : " << endl;
+    cin >> p;
+    char a;
+    cout<<"Enter a operator : " << endl;
+    cin >> a;
+    switch(a){
+        case '+': cout << n+p << endl;break;
+        case '-': cout << n-p << endl;break;
+        case '/': cout << (float)n/p << endl;break;
+        case '*': cout << n*p << endl;break;
+    }
+}
