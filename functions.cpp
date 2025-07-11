de89f52316fbf30e876137e5c40ac0d9a5321bd4
Shivam Lohia <contactshivamlohia@gmail.com>
2025-07-11 08:23:36 +0530

    Add files via upload

---
+++ functions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void print(){
+    cout << "Hello C++" << endl;
+}
+
+int sum(int a, int b){
+    return a + b;
+}
+int main(){
+    cout << sum(10,20)<<endl;
+    print();
+}
