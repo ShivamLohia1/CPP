fed77e8a5f7796840dae5b9f6af6957040c737c6
Shivam Lohia <contactshivamlohia@gmail.com>
2025-08-12 17:45:44 +0530

    Update c3.cpp

--- c3.cpp
+++ c3.cpp
@@ -4,10 +4,10 @@ int main(){
     // string str;
     // cin>>str;
     // cout<<"My name is "<<str<<". I am a Google developer.\n";
-
     //getline()
     string str2;
     cout<<"Enter your name: ";
     getline(cin,str2);     //takes strings including " " (spaces)
     cout<<"Hello "<<str2;
+
 }
