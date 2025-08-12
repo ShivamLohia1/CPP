809aae65c4e2ba3dc8c600ba330fa83aecdedcdc
Shivam Lohia <contactshivamlohia@gmail.com>
2025-08-12 17:48:22 +0530

    Update codes.cpp

--- codes.cpp
+++ codes.cpp
@@ -5,19 +5,15 @@
 
 // int main() {
 //     int n, a = 0, b = 1, c;
-
 //     cout << "Enter the number of terms: ";
 //     cin >> n;
-
 //     cout << "Fibonacci Series: ";
-
 //     for (int i = 1; i <= n; ++i) {
 //         cout << a << " ";
 //         c = a+b;
 //         a = b;
 //         b = c;
 //     }
-
 //     cout << endl;
 //     return 0;
 // }
@@ -280,4 +276,5 @@
         
 //     }
 // }
+
 
