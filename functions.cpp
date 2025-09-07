54e95d368ae2174ba2386815044ef5ba17c64d70
Shivam Lohia <contactshivamlohia@gmail.com>
2025-09-07 11:21:49 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -4,11 +4,11 @@ using namespace std;
 void print(){
     cout << "Hello C++" << endl;
 }
-
 int sum(int a, int b){
     return a + b;
 }
 int main(){
     cout << sum(10,20)<<endl;
     print();
+
 }
